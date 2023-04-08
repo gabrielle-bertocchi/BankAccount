e1ea5b27698ce5597afc70f5150a5aa1381cf2a6
Gabrielle Bertocchi Sales <99894790+gabrielle-bertocchi@users.noreply.github.com>
2023-04-08 17:28:49 +0100

    add files

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        String nome = "Jonathan Sales";
+        String tipoAccount = "Corrente";
+        double saldo = 1599.99;
+
+        System.out.println("*******************************");
+        System.out.println("\nNome do Cliente: " + nome);
+        System.out.println("Tipo da Conta: " + tipoAccount);
+        System.out.println("Saldo atual: " + saldo);
+        System.out.println("\n*******************************");
+
+
+    }
+}
